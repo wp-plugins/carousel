1fdea43d34bf02cfd4470ff4e93a7de5d0a8dff8
themepoints <themepoints@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-14 14:16:00 +0000

    Initial release
    git-svn-id: https://plugins.svn.wordpress.org/carousel/trunk@1112595 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ carousel-shortcodes.php
@@ -0,0 +1,115 @@
+<?php
+
+/*
+Plugin Name:Carousels Ultimate
+Plugin URI: http://themepoints.com
+Description: Kento Carousel Description
+Version: 1.0
+Author: themepoints
+Author URI: http://themepoints.com
+License URI: http://themepoints.com/copyright/
+*/
+if ( ! defined( 'ABSPATH' ) )
+	die( "Can't load this file directly" );
+	
+define('THEMEPOINTS_CAROUSEL_PLUGIN_PATH', WP_PLUGIN_URL . '/' . plugin_basename( dirname(__FILE__) ) . '/' );
+
+
+
+/* carousels ultimate stylesheet */
+function tp_ultimate_carousel_script()
+	{
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('tp-carousel-js', plugins_url( '/js/owl.carousel.js', __FILE__ ), array('jquery'), '1.0', false);
+	wp_enqueue_style('tp-carousel-css', THEMEPOINTS_CAROUSEL_PLUGIN_PATH.'css/owl.carousel.css');	
+	wp_enqueue_style('tp-carousel-theme-css', THEMEPOINTS_CAROUSEL_PLUGIN_PATH.'css/owl.theme.css');
+	wp_enqueue_style('wp-color-picker');
+	wp_enqueue_script('tp-carousel-wp-color-picker', plugins_url(), array( 'wp-color-picker' ), false, true );
+	}
+add_action('init', 'tp_ultimate_carousel_script');
+
+
+
+
+
+
+function tp_ultimate_carousel_js_active(){?>
+
+    <script type="text/javascript">
+    jQuery(document).ready(function() {
+      jQuery("#owl-demo").owlCarousel({
+        autoPlay: true,
+        items : 4,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3]
+      });
+
+    });
+    </script>
+<?php
+	}
+add_action('wp_head', 'tp_ultimate_carousel_js_active');
+
+
+function tp_ultimate_carousel_images($atts, $content = null) {
+	return ('<div class="item">
+				<img src="'.$content.'" alt=""/>
+			</div>	
+			');
+}
+add_shortcode ("carouselsimages", "tp_ultimate_carousel_images");
+
+
+/* carousels ultimate Shortcode */
+function tp_ultimate_carousel_shortcodes($atts, $content = null) {
+	return ('
+			<div id="demo">
+				<div class="container">
+					<div class="row">
+						<div class="span12">
+							<div id="owl-demo" class="owl-carousel">
+								'.do_shortcode($content).'
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		');
+}
+add_shortcode ("ultimatecarousels", "tp_ultimate_carousel_shortcodes");
+
+
+
+/* Add Slider Shortcode Button on Post Visual Editor */
+function themepoints_button_function() {
+   if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') )
+     return;
+   if ( get_user_option('rich_editing') == 'true') {
+	add_filter ("mce_external_plugins", "themepoints_button_js");
+	add_filter ("mce_buttons", "themepoints_button");
+   }
+	
+	
+	
+	
+
+}
+
+function themepoints_button_js($plugin_array) {
+	$plugin_array['pointscarousels'] = plugins_url('inc/shortcode.js', __FILE__);
+	return $plugin_array;
+}
+
+function themepoints_button($buttons) {
+	array_push ($buttons, 'ultimatecarousels');
+	return $buttons;
+}
+add_action ('init', 'themepoints_button_function'); 
+
+
+
+
+
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,67 @@
+=== Carousel Ultimate ===
+Contributors: themepoints
+Donate link:http://themepoints.com
+Tags:  carousel, carousel's , carousel shortcode , responsive carousel, wordpress carousel, carousel logo, logo carousel, image carousel, carousel gallery, touch carousel, easy carousel, gallery carousel, nice carousel, client carousel.
+Requires at least: 3.8
+Tested up to: 4.1.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Easy Responsive Shortcode carousel for WordPress.
+
+== Description ==
+This plugin allows you to use shortcode to display carousel post or page. you can easily create carousel using shortcode.
+
+
+### Carousel by http://themepoints.com
+
+* [Live demo!&raquo;](http://themepoints.com)
+
+<strong>Plugin Features </strong>
+* Shortcode System.
+* Touch Enable.
+* Fully responsive.
+* All Browser support.
+* Auto play Enable.
+* Unlimited Carousel.
+* Use via short-code.
+* Support post or page.
+* Enable tinymce button.
+* And many more.
+
+
+
+== Installation ==
+
+1. Install as regular WordPress plugin.
+2. Go your Pluings setting via WordPress Dashboard and activate it.
+3. After activate plugin you will see a button on the post or page visual editor called Add Carousel Shortcode. Click on it, from there you will receive the shortcode.<br />
+<br /><br />
+
+<strong>How to use short-code?</strong><br />
+
+
+<br /><br />
+
+
+
+
+
+== Screenshots ==
+
+1. screenshot-1
+2. screenshot-2
+3. screenshot-3
+
+== Upgrade Notice ==
+
+
+== Frequently Asked Questions ==
+
+
+== Changelog ==
+
+
+= 1.0 =
+* Initial release
